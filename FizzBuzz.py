0899f3c4a081d0c29c26294da1bfb684c43f5e3d
Leonides <leonides1998@gmail.com>
2022-10-11 04:30:01 +0800

    Version1

--- FizzBuzz.py
+++ FizzBuzz.py
@@ -1,1 +1,12 @@
-print("HelloWorld-Test")
+num = 1
+
+while num <101:
+    if(num %3 ==0 and num %5 ==0 ):
+        print("FizzBuzz")
+    elif(num %3 ==0):
+        print("Fizz")
+    elif(num %5 ==0):
+        print("Buzz")
+    else:
+        print(num)
+    num=num+1
